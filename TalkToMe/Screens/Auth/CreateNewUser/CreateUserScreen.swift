7c4ae9226df7e6a5c657375defea63584c036185
Vlad <160039489+55kt@users.noreply.github.com>
2024-08-05 19:21:02 +0300

    submit button

--- TalkToMe/Screens/Auth/CreateNewUser/CreateUserScreen.swift
+++ TalkToMe/Screens/Auth/CreateNewUser/CreateUserScreen.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct CreateUserScreen: View {
     // MARK: - Properties
-    @State var username: String
+    @State private var keyboardHeight: CGFloat = 0
+    @State private var textFieldContent: String = ""
     
     // MARK: - Body
     var body: some View {
         ZStack {
             // Background elements
             IntroGradient()
-            //            OpacityBackgroundLogo()Us
             
             // Nav text area
             VStack {
@@ -37,13 +37,13 @@ struct CreateUserScreen: View {
             .multilineTextAlignment(.center)
             .shadow(radius: 10)
             
-            
+            // Avatar Area
             VStack {
                 ZStack {
                     Circle()
                         .padding()
                         .scaledToFit()
-                        .frame(width: 230, height: 230)
+                        .frame(width: 190, height: 190)
                         .foregroundStyle(.white)
                         .opacity(0.5)
                         .shadow(radius: 10)
@@ -52,18 +52,23 @@ struct CreateUserScreen: View {
                     Image(systemName: "person.fill")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 130, height: 130)
+                        .frame(width: 100, height: 100)
                 }
             }
-            .padding(.bottom, 140)
+            .padding(.bottom, 160)
             
             HStack {
                 Image(systemName: "person")
                     .fontWeight(.semibold)
                     .frame(width: 30)
                 
-                TextField("Username", text: $username).font(.title3)
+                TextField("Username", text: $textFieldContent).font(.title3)
                     .shadow(radius: 10)
+                    .onChange(of: textFieldContent) {oldValue, newValue in
+                        if newValue.count > 15 {
+                            textFieldContent = String(newValue.prefix(15))
+                        }
+                    }
                 
             }
             .foregroundStyle(.white)
@@ -71,20 +76,44 @@ struct CreateUserScreen: View {
             .background(Color.white.opacity(0.3))
             .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
             .padding(.horizontal, 15)
-            .padding(.top, 150)
-        }
-        .gesture(
-            DragGesture(minimumDistance: 100, coordinateSpace: .local)
-                .onEnded { value in
-                    if value.translation.height > 0 {
-                        hideKeyboard()
-                    }
+            .padding(.top, 90)
+            
+            
+            VStack {
+                Spacer()
+                Button {
+                    print("Button tapped!")
+                } label: {
+                    Text("Submit")
+                        .foregroundColor(.black)
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(.vertical, 18)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(textFieldContent.count == 15 ? Color.yellow : Color.yellow.opacity(0.3))
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .padding(.horizontal, 16)
+                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: -5)
                 }
-        )
+                .disabled(textFieldContent.isEmpty || textFieldContent.count != 15)
+                .frame(height: 100)
+            }
+        }
+        .ignoresSafeArea(.keyboard)
+        .onTapGesture {
+            hiderKeyboard()
+        }
+        .animation(.easeOut(duration: 0.3), value: keyboardHeight)
     }
 }
 
 // MARK: - Preview
 #Preview {
-    CreateUserScreen(username: "")
+    CreateUserScreen()
+}
+
+// MARK: - Hide Keyboard Function
+extension View {
+    func hiderKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
