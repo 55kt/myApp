ee439b4893b47339333ba96691c831377ea7b611
Vlad <160039489+55kt@users.noreply.github.com>
2024-08-12 18:11:43 +0300

    image picker user avatar

--- TalkToMe/Screens/AuthScreens/OTPRegistration/Views/CreateNewUser/SubViews/UserAvatar.swift
+++ TalkToMe/Screens/AuthScreens/OTPRegistration/Views/CreateNewUser/SubViews/UserAvatar.swift
@@ -1,27 +1,55 @@
 import SwiftUI
+import PhotosUI
 
 struct UserAvatar: View {
+    @State private var userAvatar: UIImage?
+    @State private var photosPickerItem: PhotosPickerItem?
     
     var body: some View {
         VStack {
             ZStack {
-                Circle()
-                    .padding()
-                    .scaledToFit()
-                    .frame(width: 180, height: 180)
-                    .foregroundStyle(.white)
-                    .opacity(0.5)
-                    .shadow(radius: 10)
-                
-                
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .foregroundStyle(.white)
-
+                PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                    if let userAvatar = userAvatar {
+                        Image(uiImage: userAvatar)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 150, height: 150)
+                            .clipShape(Circle())
+                            .shadow(radius: 10)
+                    } else {
+                        defaultAvatar() // Вызов метода для отображения дефолтного аватара
+                    }
+                }
             }
         }
         .padding(.bottom, 220)
+        .onChange(of: photosPickerItem) { _, _ in
+            Task {
+                if let photosPickerItem, let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                    if let image = UIImage(data: data) {
+                        userAvatar = image
+                    }
+                }
+            }
+        }
+    }
+    
+    // Метод для отображения дефолтного аватара
+    func defaultAvatar() -> some View {
+        ZStack {
+            Circle()
+                .padding()
+                .scaledToFit()
+                .frame(width: 180, height: 180)
+                .foregroundStyle(.white)
+                .opacity(0.5)
+                .shadow(radius: 10)
+            
+            Image(systemName: "person.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.white)
+        }
     }
 }
