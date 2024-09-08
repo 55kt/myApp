880955448129970de0cb95ec2fe0a09158e19731
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-08 19:00:31 +0300

    Setting Up Our Firebase Uploader

--- TalkToMe/Utils/FirebaseConstants.swift
+++ TalkToMe/Utils/FirebaseConstants.swift
@@ -3,6 +3,8 @@ import Firebase
 import FirebaseStorage
 
 enum FirebaseConstants {
+    static let StorageRef = Storage.storage().reference()
+    
     private static let DatabaseRef = Database.database().reference()
     static let UserRef = DatabaseRef.child("users")
     static let ChannelsRef = DatabaseRef.child("channels")
