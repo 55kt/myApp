86d161725990c41a628a6da2191dae89fce20897
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-12 20:49:47 +0300

    Paginating ChatRoom Screen Messages

--- TalkToMe/Service/MessageService.swift
+++ TalkToMe/Service/MessageService.swift
@@ -1,4 +1,6 @@
 import Foundation
+import Firebase
+import FirebaseDatabase
 
 // MARK: Handles sending and fetching messages and setting reactions
 struct MessageService {
@@ -57,7 +59,7 @@ struct MessageService {
         FirebaseConstants.MessagesRef.child(channel.id).child(messageId).setValue(messageDict)
         
         completion()
-
+        
     }
     
     static func getMessages(for channel: ChannelItem, completion: @escaping([MessageItem]) -> Void) {
@@ -79,6 +81,50 @@ struct MessageService {
             print("Failed to get messages: \(channel.title)")
         }
     }
+    
+    static func getHistoricalMessages(for channel: ChannelItem, lastCursor: String?, pageSize: UInt, completion: @escaping (MessageNode) -> Void) {
+        
+        let query: DatabaseQuery
+        
+        if lastCursor == nil {
+            query = FirebaseConstants.MessagesRef.child(channel.id).queryLimited(toLast: pageSize)
+        } else {
+            query = FirebaseConstants.MessagesRef.child(channel.id)
+                .queryOrderedByKey()
+                .queryEnding(atValue: lastCursor)
+                .queryLimited(toLast: pageSize)
+        }
+        
+        query.observeSingleEvent(of: .value) { mainSnapshot in
+            guard let first = mainSnapshot.children.allObjects.first as? DataSnapshot,
+                  let allObjects = mainSnapshot.children.allObjects as? [DataSnapshot]
+            else { return }
+            var messages: [MessageItem] = allObjects.compactMap { messageSnapshot in
+                let messageDict = messageSnapshot.value as? [String: Any] ?? [:]
+                var message = MessageItem(id: messageSnapshot.key, isGroupChat: channel.isGroupChat, dict: messageDict)
+                let messageSender = channel.members.first(where: { $0.uid == message.ownerUid })
+                message.sender = messageSender
+                return message
+            }
+            
+            messages.sort { $0.timeStamp < $1.timeStamp }
+            
+            if messages.count == mainSnapshot.childrenCount {
+                let messageNode = MessageNode(messages: messages, currentCursor: first.key)
+                completion(messageNode)
+            }
+            
+        } withCancel: { error in
+            print("Failed to get messages for channel: \(channel.name)")
+            completion(.emptyNode)
+        }
+    }
+}
+
+struct MessageNode {
+    var messages: [MessageItem]
+    var currentCursor: String?
+    static let emptyNode = MessageNode(messages: [], currentCursor: nil)
 }
 
 struct MessageUploadParams {
